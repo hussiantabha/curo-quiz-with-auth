67996904f06eeca1dd6f7f7d6daee71b0fad26ae
hussiantabha <tabhahusain@gmail.com>
2022-05-24 17:51:41 +0530

    Added: 3 Quiz
    Movies, Sports, Gadget quiz with functionalities like display, collecting answers

---
+++ src/pages/Results.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Results = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Results</div>
+    </>
+  );
+};
+
+export default Results;
